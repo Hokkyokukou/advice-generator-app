2b1d4b762c73e4f04cadd8f3704621f171bf6488
Hokkyokukou <hokkyokukou27@gmail.com>
2023-04-17 14:22:20 +0200

    Add engine.js

---
+++ assets/js/engine.js
@@ -0,0 +1,5 @@
+const API_LINK = "https://api.adviceslip.com/advice";//API
+const adviceNumber = document.querySelector('.number');//ADVICE #117
+const adviceQuoteArea = document.querySelector('quote');//quote
+const fetchBtn = document.querySelector('button.btn');//btn dice
+
