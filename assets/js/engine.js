3ecd36c1d2947f78355571ae4f42f64525b3bb14
Hokkyokukou <hokkyokukou27@gmail.com>
2023-04-20 20:19:28 +0200

    Update engine.js

--- assets/js/engine.js
+++ assets/js/engine.js
@@ -1,5 +1,31 @@
-const API_LINK = "https://api.adviceslip.com/advice";//API
-const adviceNumber = document.querySelector('.number');//ADVICE #117
-const adviceQuoteArea = document.querySelector('quote');//quote
-const fetchBtn = document.querySelector('button.btn');//btn dice
+const myAdviceGenerator = () => {
+    const API_LINK = 'https://api.adviceslip.com/advice'; //API
+    const adviceNumber = document.querySelector('.advice_number'); //ADVICE #117
+    const adviceQuoteArea = document.querySelector('.quote'); //quote
+    const fetchBtn = document.querySelector('button.btn'); //btn dice
 
+    const fetchNewAdvice = async () => {
+        const response = await fetch(API_LINK);
+        const advice = await response.json();
+        return advice;
+    };
+
+    const renderAdvice = (adviceObj) => {
+        const { id, advice } = adviceObj;
+        adviceNumber.textContent = `ADVICE #${id}`;
+        adviceQuoteArea.textContent = `“${advice}”`;
+    };
+
+    const generateNewAdvice = async () => {
+        const data = await fetchNewAdvice();
+        const adviceSlip = data.slip;
+        // console.log(adviceSlip);
+        renderAdvice(adviceSlip);
+    };
+
+    this.addEventListener('DOMContentLoaded', () => {
+        fetchBtn.addEventListener('click', generateNewAdvice);
+    });
+};
+
+myAdviceGenerator();
